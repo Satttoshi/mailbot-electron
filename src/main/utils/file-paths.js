e4ae1a791567ca8e8c56ec0b25a6dd56bc622048
Joshua Stolle <joshuastolle445@gmail.com>
2024-04-06 22:09:12 +0200

    feat: utils for managing file paths
    The file file-paths.js was added under src/main/utils/ directory. This file is meant to manage all paths to files throughout the application, helping to ensure better organization and accessibility to file paths. It includes paths for contentFile, app, and privateConfigFile.

---
+++ src/main/utils/file-paths.js
@@ -0,0 +1,17 @@
+import { app } from 'electron'
+import path from 'path'
+import fileConfig from '../../../file.config'
+
+const isDevelopment = !app.isPackaged
+
+export const appPath = isDevelopment
+  ? path.join(app.getAppPath())
+  : path.join(process.resourcesPath)
+
+export const contentFilePath = path.join(appPath, 'resources', `${fileConfig.contentFileName}.txt`)
+
+export const privateConfigFilePath = path.join(
+  appPath,
+  'resources',
+  `${fileConfig.privateConfigFileName}.json`
+)
