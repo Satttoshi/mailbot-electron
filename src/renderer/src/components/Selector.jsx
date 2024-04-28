084e3ee77c232b72046c4b326381e35d54cd8967
Joshua Stolle <joshuastolle445@gmail.com>
2024-04-28 22:33:51 +0200

    feat: Selector component style
    Enhanced the user interface of the Selector component by adding hover styles for better user interaction. The change increases the visual feedback for users when hovering over the selectable field.

--- src/renderer/src/components/Selector.jsx
+++ src/renderer/src/components/Selector.jsx
@@ -13,7 +13,7 @@ const Selector = () => {
     <div className="flex flex-row py-2 px-4 bg-purple-900 items-center justify-center rounded flex-wrap">
       <label className="font-medium text-white whitespace-nowrap">Sending from:</label>
       <select
-        className="form-select text-center appearance-none block w-full my-1 px-2 py-1 text-base font-medium text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        className="form-select text-center appearance-none block w-full my-1 px-2 py-1 text-base font-medium text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none hover:cursor-pointer hover:bg-slate-200"
         value={selectedMailIndex}
         onChange={handleChange}
       >
