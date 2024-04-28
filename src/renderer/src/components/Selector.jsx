5adb23245224f1ee56dc8fa82f06b8313972763d
Joshua Stolle <joshuastolle445@gmail.com>
2024-04-28 19:08:52 +0200

    refactor: Selector and update Button styles
    The 'Selector' component was refactored to use a native select component instead of a custom button component. As a consequence, the 'dynamicBorder' style variant was removed from the 'Button' component. Additionally, the height property of the 'ConsoleTextarea' component was changed to maximum height to adjust to the content more effectively.

--- src/renderer/src/components/Selector.jsx
+++ src/renderer/src/components/Selector.jsx
@@ -1,23 +1,28 @@
 import { useStore } from '../hooks/useStore';
-import Button from './Button';
 
 const Selector = () => {
   const mailNames = useStore((state) => state.mailNames);
   const selectedMailIndex = useStore((state) => state.selectedMailIndex);
   const setSelectedMailIndex = useStore((state) => state.setSelectedMailIndex);
 
+  const handleChange = (event) => {
+    setSelectedMailIndex(parseInt(event.target.value, 10));
+  };
+
   return (
-    <div className="flex flex-row gap-4 items-center justify-center py-4 flex-wrap">
-      {mailNames.map((element, index) => (
-        <Button
-          key={index}
-          index
-          selected={selectedMailIndex}
-          label={element}
-          variant="dynamicBorder"
-          onClick={() => setSelectedMailIndex(index)}
-        />
-      ))}
+    <div className="flex flex-row py-2 px-4 bg-purple-900 items-center justify-center rounded flex-wrap">
+      <label className="font-medium text-white whitespace-nowrap">Sending from:</label>
+      <select
+        className="form-select text-center appearance-none block w-full my-1 px-2 py-1 text-base font-medium text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        value={selectedMailIndex}
+        onChange={handleChange}
+      >
+        {mailNames.map((element, index) => (
+          <option key={index} value={index}>
+            {element}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
