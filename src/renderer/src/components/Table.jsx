7caaf4004a932f2c5154af14f4367cfa5f358c23
Joshua Stolle <joshuastolle445@gmail.com>
2024-05-05 12:32:28 +0200

    feat: undo/redo Table options
    Table component was updated to handle email data instead of car information. Added features include the ability to edit cells, with undo and redo options for cell edits, as well as supporting multiple row selection. Also, the component's styling was readjusted to accommodate these changes.

--- src/renderer/src/components/Table.jsx
+++ src/renderer/src/components/Table.jsx
@@ -7,23 +7,40 @@ import { useState } from 'react'; // Optional Theme applied to the grid
 const Table = () => {
   // Row Data: The data to be displayed.
   const [rowData, setRowData] = useState([
-    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false }
+    { '#': 0, emails: 'maxdefaultman0@gmail.com', sent: true },
+    { '#': 1, emails: 'maxdefaultman1@gmail.com', sent: false },
+    { '#': 2, emails: 'maxdefaultman2@gmail.com', sent: false },
+    { '#': 3, emails: 'maxdefaultman0@gmail.com', sent: false },
+    { '#': 4, emails: 'maxdefaultman1@gmail.com', sent: false },
+    { '#': 5, emails: 'maxdefaultman2@gmail.com', sent: false },
+    { '#': 6, emails: 'maxdefaultman0@gmail.com', sent: false },
+    { '#': 7, emails: 'maxdefaultman1@gmail.com', sent: false },
+    { '#': 8, emails: 'maxdefaultman2@gmail.com', sent: false },
+    { '#': 9, emails: 'maxdefaultman0@gmail.com', sent: false },
+    { '#': 10, emails: 'maxdefaultman1@gmail.com', sent: false },
+    { '#': 11, emails: 'maxdefaultman2@gmail.com', sent: false }
   ]);
 
   // Column Definitions: Defines the columns to be displayed.
   const [colDefs, setColDefs] = useState([
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
-    { field: 'electric' }
+    { field: '#', width: 80 },
+    { field: 'emails', width: 300, editable: true },
+    { field: 'sent', width: 120 }
   ]);
+
+  const undoRedoCellEditing = true;
+  const undoRedoCellEditingLimit = 20;
 
   return (
     // wrapping container with theme & size
-    <div className="ag-theme-quartz w-full p-2" style={{ height: 500 }}>
-      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    <div className="ag-theme-quartz w-full h-full p-4">
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+        rowSelection="multiple"
+        undoRedoCellEditing={undoRedoCellEditing}
+        undoRedoCellEditingLimit={undoRedoCellEditingLimit}
+      />
     </div>
   );
 };
