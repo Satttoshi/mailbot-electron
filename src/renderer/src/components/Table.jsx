84d35e2aebcdec88ba94e1ae51110acd5f7c8cab
Joshua Stolle <joshuastolle445@gmail.com>
2024-05-05 16:19:05 +0200

    feat: bulk paste handling and cell editing to Table component
    The Table component has been enhanced with the ability to handle paste events as well as editing of cells. New state variables (isEditing, currentSelection) have been added and life-cycle methods (useCallback, useEffect) are now employed. The resultant effect is more flexible data manipulation for users within the table.

--- src/renderer/src/components/Table.jsx
+++ src/renderer/src/components/Table.jsx
@@ -2,9 +2,12 @@ import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import 'ag-grid-community/styles/ag-grid.css'; // Mandatory CSS required by the grid
 import 'ag-grid-community/styles/ag-theme-quartz.css';
 import '../assets/ag-grid-custom-icons.css';
-import { useState } from 'react'; // Optional Theme applied to the grid
+import { useCallback, useEffect, useState } from 'react'; // Optional Theme applied to the grid
 
 const Table = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentSelection, setCurrentSelection] = useState(0);
+
   // Row Data: The data to be displayed.
   const [rowData, setRowData] = useState([
     { '#': 0, emails: 'maxdefaultman0@gmail.com', sent: true },
@@ -22,21 +25,56 @@ const Table = () => {
   ]);
 
   // Column Definitions: Defines the columns to be displayed.
-  const [colDefs, setColDefs] = useState([
+  const colDefs = [
     { field: '#', width: 80 },
     { field: 'emails', width: 300, editable: true },
     { field: 'sent', width: 120 }
-  ]);
+  ];
 
   const undoRedoCellEditing = true;
   const undoRedoCellEditingLimit = 20;
 
+  const handlePaste = useCallback(
+    (event) => {
+      if (!isEditing) return;
+
+      const paste = event.clipboardData.getData('text');
+      const rows = paste
+        .split('\n')
+        .map((emails) => emails.split('\t'))
+        .map((row, index) => {
+          return {
+            '#': currentSelection + index,
+            emails: row[0].replace(/\r/g, ''),
+            sent: false
+          };
+        });
+
+      const newData = [...rowData];
+      newData.splice(currentSelection, rows.length, ...rows);
+
+      setRowData(newData);
+    },
+    [rowData, isEditing]
+  );
+
+  useEffect(() => {
+    document.addEventListener('paste', handlePaste);
+    return () => {
+      document.removeEventListener('paste', handlePaste);
+    };
+  }, [handlePaste]);
+
   return (
-    // wrapping container with theme & size
     <div className="ag-theme-quartz w-full h-full p-4">
       <AgGridReact
         rowData={rowData}
         columnDefs={colDefs}
+        onCellEditingStarted={(params) => {
+          setCurrentSelection(params.rowIndex);
+          setIsEditing(true);
+        }}
+        onCellEditingStopped={() => setIsEditing(false)}
         rowSelection="multiple"
         undoRedoCellEditing={undoRedoCellEditing}
         undoRedoCellEditingLimit={undoRedoCellEditingLimit}
