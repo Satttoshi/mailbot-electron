6956c355a8fd24bfbe5c5878add750e6e9718c64
Joshua Stolle <joshuastolle445@gmail.com>
2024-05-10 15:41:01 +0200

    fix: add shouldShutdown in dependency array

--- src/renderer/src/hooks/useIPCEvents.js
+++ src/renderer/src/hooks/useIPCEvents.js
@@ -44,7 +44,7 @@ export const useIPCEvents = () => {
       messageListener();
       triggerListener();
     };
-  }, []);
+  }, [shouldShutdown]);
 
   return { runMailer, writeMailContentToTxt };
 };
